5c992f29070b896e31d38b26be3b334168dd198c
Justin Crawford <Justin@stacksmash.net>
2020-12-18 03:57:37 -0800

    cameraservice: rename aosp to evolution for client_package_name
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2966,8 +2966,8 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateOpenCloseStatus(mCameraIdStr, true/*open*/, mClientPackageName);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/misc/aosp/client_package_name");
-    std::string cpn = String8(mClientPackageName).string();
+	std::ofstream cpf("/data/misc/evolution/client_package_name");
+	std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif
 

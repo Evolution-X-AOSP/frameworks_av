4ea20de7b176499768b7f836ff01b47868b2ff72
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2021-10-28 22:23:56 +0000

    audioflinger: Call resetVolume before enabling effect
    In EffectModule::start() before enabling an audio effect, call
    resetVolume to ensure effect gets the correct volume before it
    starts to process data.
    
    Bug: 202360137
    Test: YT Music with steps in bug
    
    Change-Id: Ic33242bd7337e9c90c49782e487e37155f4125c5

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -1119,6 +1119,8 @@ void AudioFlinger::EffectModule::addEffectToHal_l()
 // start() must be called with PlaybackThread::mLock or EffectChain::mLock held
 status_t AudioFlinger::EffectModule::start()
 {
+    // set volume before enabling an effect
+    getCallback()->resetVolume();
     status_t status;
     {
         Mutex::Autolock _l(mLock);

aa328818f0ac82f9efef8344266b19c8c5ff1886
SGCMarkus <markusornik@gmail.com>
2022-10-09 16:12:50 +0200

    Camera: Avoid roundBufferDimensionsNearest also for HAL_PIXEL_FORMAT_BLOB
    This fixes the Ultra-Res feature with Motorolas MotCamera3
    It uses multiple camera streams, with only the blob format using the
    full sensor size (e.g. on berlin: 12000 x 9000 for 108 MP)
    
    Change-Id: I025e068b2d3ff1123599ae5655655876ee28f39b
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- services/camera/libcameraservice/utils/SessionConfigurationUtils.cpp
+++ services/camera/libcameraservice/utils/SessionConfigurationUtils.cpp
@@ -155,7 +155,7 @@ int64_t euclidDistSquare(int32_t x0, int32_t y0, int32_t x1, int32_t y1) {
 bool roundBufferDimensionNearest(int32_t width, int32_t height,
         int32_t format, android_dataspace dataSpace,
         const CameraMetadata& info, bool maxResolution, /*out*/int32_t* outWidth,
-        /*out*/int32_t* outHeight, bool isPriviledgedClient) {
+        /*out*/int32_t* outHeight, bool isPrivilegedClient) {
     const int32_t depthSizesTag =
             getAppropriateModeTag(ANDROID_DEPTH_AVAILABLE_DEPTH_STREAM_CONFIGURATIONS,
                     maxResolution);
@@ -201,7 +201,7 @@ bool roundBufferDimensionNearest(int32_t width, int32_t height,
         }
     }
 
-    if (isPriviledgedClient == true && bestWidth == -1 &&
+    if (isPrivilegedClient == true && bestWidth == -1 &&
         (format == HAL_PIXEL_FORMAT_RAW10 || format == HAL_PIXEL_FORMAT_RAW12 ||
          format == HAL_PIXEL_FORMAT_RAW16 || format == HAL_PIXEL_FORMAT_RAW_OPAQUE)) {
         bool isLogicalCamera = false;
@@ -223,10 +223,11 @@ bool roundBufferDimensionNearest(int32_t width, int32_t height,
     // Avoid roundBufferDimensionsNearest for privileged client YUV streams to meet the AIDE2
     // requirement. AIDE2 is vendor enhanced feature which requires special resolutions and
     // those are not populated in static capabilities.
-    if (isPriviledgedClient == true && format == HAL_PIXEL_FORMAT_YCbCr_420_888) {
+    if (isPrivilegedClient == true &&
+        (format == HAL_PIXEL_FORMAT_YCbCr_420_888 || format == HAL_PIXEL_FORMAT_BLOB)) {
         ALOGI("Bypass roundBufferDimensionNearest for privilegedClient YUV streams "
-              "width %d height %d",
-              width, height);
+              "width %d height %d for format %d",
+              width, height, format);
 
         bestWidth  = width;
         bestHeight = height;
@@ -463,7 +464,7 @@ binder::Status createSurfaceFromGbp(
         const std::string &logicalCameraId, const CameraMetadata &physicalCameraMetadata,
         const std::vector<int32_t> &sensorPixelModesUsed, int64_t dynamicRangeProfile,
         int64_t streamUseCase, int timestampBase, int mirrorMode,
-        int32_t colorSpace, bool isPriviledgedClient) {
+        int32_t colorSpace, bool isPrivilegedClient) {
     // bufferProducer must be non-null
     if (gbp == nullptr) {
         std::string msg = fmt::sprintf("Camera %s: Surface is NULL", logicalCameraId.c_str());
@@ -493,7 +494,7 @@ binder::Status createSurfaceFromGbp(
     uint64_t allowedFlags = GraphicBuffer::USAGE_SW_READ_MASK |
                            GraphicBuffer::USAGE_HW_TEXTURE |
                            GraphicBuffer::USAGE_HW_COMPOSER;
-    bool flexibleConsumer = (consumerUsage & disallowedFlags) == 0 &&
+    bool flexibleConsumer = !isPrivilegedClient && (consumerUsage & disallowedFlags) == 0 &&
             (consumerUsage & allowedFlags) != 0;
 
     surface = new Surface(gbp, useAsync);
@@ -564,7 +565,7 @@ binder::Status createSurfaceFromGbp(
     if (flexibleConsumer && isPublicFormat(format) &&
             !SessionConfigurationUtils::roundBufferDimensionNearest(width, height,
             format, dataSpace, physicalCameraMetadata, foundInMaxRes, /*out*/&width,
-            /*out*/&height, isPriviledgedClient)) {
+            /*out*/&height, isPrivilegedClient)) {
         std::string msg = fmt::sprintf("Camera %s: No supported stream configurations with "
                 "format %#x defined, failed to create output stream",
                 logicalCameraId.c_str(), format);
@@ -715,7 +716,7 @@ convertToHALStreamCombination(
         metadataGetter getMetadata, const std::vector<std::string> &physicalCameraIds,
         aidl::android::hardware::camera::device::StreamConfiguration &streamConfiguration,
         bool overrideForPerfClass, metadata_vendor_id_t vendorTagId,
-        bool checkSessionParams, bool *earlyExit, bool isPriviledgedClient) {
+        bool checkSessionParams, bool *earlyExit, bool isPrivilegedClient) {
     using SensorPixelMode = aidl::android::hardware::camera::metadata::SensorPixelMode;
     auto operatingMode = sessionConfiguration.getOperatingMode();
     binder::Status res = checkOperatingMode(operatingMode, deviceInfo,
@@ -846,7 +847,7 @@ convertToHALStreamCombination(
             sp<Surface> surface;
             res = createSurfaceFromGbp(streamInfo, isStreamInfoValid, surface, bufferProducer,
                     logicalCameraId, metadataChosen, sensorPixelModesUsed, dynamicRangeProfile,
-                    streamUseCase, timestampBase, mirrorMode, colorSpace, isPriviledgedClient);
+                    streamUseCase, timestampBase, mirrorMode, colorSpace, isPrivilegedClient);
 
             if (!res.isOk())
                 return res;

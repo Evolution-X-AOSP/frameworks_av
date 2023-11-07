cb687e4253a489a2766a7fa763636ed416a1f8ac
Joey Huab <joey@evolution-x.org>
2023-11-07 15:48:57 +0900

    Revert "Codec2: guard the dummy work signal to lahaina only"
    This reverts commit 342adae3119ed07a9acbdcf7fc25ff15b3c94d4e.

--- media/codec2/sfplugin/CCodecBufferChannel.h
+++ media/codec2/sfplugin/CCodecBufferChannel.h
@@ -370,9 +370,6 @@ private:
     uint64_t mLastInputBufferAvailableTs;
     std::mutex mTsLock;
 
-    // whether the HAL needs a dummy work
-    bool mNeedEmptyWork;
-
     sp<ICrypto> mCrypto;
     sp<IDescrambler> mDescrambler;
 
